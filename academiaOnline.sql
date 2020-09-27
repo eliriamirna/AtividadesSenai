3286d53ccf138abe9c7929982b075b884b86ff94
Eliria Mirna <eliriadias@gmail.com>
2020-09-27 10:41:08 -0300

    banco de dados cliente da academia online

---
+++ academiaOnline.sql
@@ -0,0 +1,45 @@
+create database academiaOnline;
+use academiaOnline;
+
+create table cadastro(
+idcadastro int auto_increment primary key,
+nome varchar(100) not null,
+cpf varchar(11) not null,
+endereco varchar(150) not null,
+bairro varchar(50) not null,
+cidade varchar(50) not null,
+UF char(2) not null,
+email varchar(100) not null,
+telefone bigint(14) not null,
+senha varchar(20) not null
+);
+
+create table pagamento(
+nomeCartao varchar(100) not null,
+numCartao varchar(16) not null,
+bandeira varchar(50) not null,
+validade varchar(7) not null,
+codSeguranca varchar(3)
+);
+
+create table dadosPessoais(
+nome varchar(100) not null,
+cpf varchar(11) not null,
+peso decimal(5,2) not null,
+idade int not null,
+altura decimal(3,2) not null,
+imc decimal(5,3),
+problemaSaude varchar(250) not null,
+limitacoes varchar(250) not null
+);
+
+create table treino(
+nome varchar(100) not null,
+cpf varchar(11) not null,
+inicio date not null,
+objetivoTreino varchar(200),
+frequenciaSemanal int(7),
+horaTreino time,
+duracaoTreino int,
+modalidade varchar(50)
+);
